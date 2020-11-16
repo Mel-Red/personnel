5a34ab5b4d06990e240a241ef651a71186e87e03
Mel-Red <71647239+Mel-Red@users.noreply.github.com>
2020-11-16 16:54:06 +0100

    Begin DataBases

---
+++ BDD/InsertTablePersonnel.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO employe VALUES ('000001','To','Ri','saltyboy@tank2k21.com','000001','01/04/2020','Sardoche-x-Faker');
+INSERT INTO employe VALUES('000002','Daxter','Jack','dead-dog@reddog.com','000002','20/04/2020','Echo');
+INSERT INTO employe VALUES('000003','Lands','Border','instant@mayhem.com','000003','29/02/2020','ButtStallion');
+INSERT INTO employe VALUES('000004','Clank','Ratchet','no-sleep@goodnight.com','000004','06/09/2020','Lombax');
+
+INSERT INTO ligue VALUES ('000001','Balenced-Team','000001','Mere','Trynda');
+INSERT INTO ligue VALUES ('000002','Pew-Pew-Go-Vroom','000002','Echo','Dark');
+INSERT INTO ligue VALUES ('000003','Loot.exe','000003','BoX','Gear');
+INSERT INTO ligue VALUES ('000004','Not-So-Cute','000004','III','T.E.L.T');
+
+INSERT INTO admin_ligue VALUES 
+INSERT INTO admin_ligue VALUES
+INSERT INTO admin_ligue VALUES
+INSERT INTO admin_ligue VALUES
