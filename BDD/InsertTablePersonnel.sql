3bd5ee767b06df367804d00fee88b5056dbdc203
Mel-Red <71647239+Mel-Red@users.noreply.github.com>
2020-11-23 13:12:15 +0100

    Update InsertTablePersonnel.sql

--- BDD/InsertTablePersonnel.sql
+++ BDD/InsertTablePersonnel.sql
@@ -1,8 +1,8 @@
 
-INSERT INTO employe VALUES ('000001','To','Ri','saltyboy@tank2k21.com','01/04/2020','02/04/2020','Sardoche-x-Faker', true, false);
-INSERT INTO employe VALUES('000002','Daxter','Jack','dead-dog@reddog.com','19/04/2020','20/04/2020','Echo', true, false);
-INSERT INTO employe VALUES('000003','Lands','Border','instant@mayhem.com','28/02/2020','29/02/2020','ButtStallion', true, true);
-INSERT INTO employe VALUES('000004','Clank','Ratchet','no-sleep@goodnight.com','05/09/2020','06/09/2020','Lombax', false, false);
+INSERT INTO employe VALUES ('000001','To','Ri','saltyboy@tank2k21.com','01-04-2020','02-04-2020','Sardoche-x-Faker', true, false);
+INSERT INTO employe VALUES('000002','Daxter','Jack','dead-dog@reddog.com','19-04-2020','20-04-2020','Echo', true, false);
+INSERT INTO employe VALUES('000003','Lands','Border','instant@mayhem.com','28-02-2020','29-02-2020','ButtStallion', true, true);
+INSERT INTO employe VALUES('000004','Clank','Ratchet','no-sleep@goodnight.com','05-09-2020','06-09-2020','Lombax', false, false);
 
 INSERT INTO ligue VALUES ('000001','Balenced-Team');
 INSERT INTO ligue VALUES ('000002','Pew-Pew-Go-Vroom');
