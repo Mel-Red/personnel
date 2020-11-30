c409587b31ec4ef4fe39aa5d2cb3a4ed3a20889c
Mel-Red <71647239+Mel-Red@users.noreply.github.com>
2020-11-30 16:50:46 +0100

    Multichanges on MCD and scripts

--- BDD/CreatePersonnel.sql
+++ BDD/CreatePersonnel.sql
@@ -2,7 +2,19 @@ DROP DATABASE personnel;
 CREATE DATABASE personnel;
 USE personnel;
 
-CREATE TABLE employe
+CREATE TABLE employe1
+(
+	idE int(6),
+	nomE varchar(15),
+	prenomE varchar(15),
+	mailE varchar(50),
+	DateA Date, 
+	DateD Date,
+	MdpE varchar(50)
+)
+ENGINE=INNODB;
+
+CREATE TABLE employe2
 (
 	idE int(6),
 	nomE varchar(15),
@@ -11,15 +23,13 @@ CREATE TABLE employe
 	DateA Date, 
 	DateD Date,
 	MdpE varchar(50),
-	EstAdmin boolean,
-	EstRoot boolean,
 	idLigue int(6)
 )
-ENGINE=INNODB;
 
 CREATE TABLE ligue
 (
 	idL int(6),
-	nomL varchar(25)
+	nomL varchar(25),
+	idAdmin int(6)
 )
 ENGINE=INNODB;
